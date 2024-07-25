25ec9e0fd8fe611c205fba1522bb03f19a1c113e
Ano <fernandobruzzi6@gmail.com>
2024-07-25 17:56:14 -0300

    adicionando pasta com data set e prog paraanalisar

---
+++ Probabilidade Condicional com Data Set/estudo_p_condicional.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = 'ic/Probabilidade Condicional com Data Set/GeoSymbio e infos.xlsx'
+df = pd.read_excel(io = path)
+
+# print(df.head())
+# print(df.info())
+
